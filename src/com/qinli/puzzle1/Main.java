7bd13cfabcc2d5553ded99d7e5ecd8e67e6f594b
fusu0623 <78517182+fusu0623@users.noreply.github.com>
2021-02-04 22:29:17 +0800

    Update Main.java

--- src/com/qinli/puzzle1/Main.java
+++ src/com/qinli/puzzle1/Main.java
@@ -5,22 +5,23 @@ package com.qinli.puzzle1;
  * @Author Cambria
  * @creat 2021/2/3 23:29
  */
-public class Main {
-    static Student[] students = new Student[4];
+class Main {
+    static Student[] students = new Student[3];
 
     public static void main(String[] args) {
 
-        students[1] = new Student("法外狂徒张三",20);
-        students[2] = new Student("李四",21);
-        students[3] = new Student("王五",19);
+        students[0] = new Student("法外狂徒张三",20);
+        students[1] = new Student("李四",21);
+        students[2] = new Student("王五",19);
 
         printStu();
     }
 
     static void printStu(){
-        for (int i=1;i<=3;i++) {
-            System.out.println("姓名：" + students[i].name);
-            System.out.println("年龄：" + students[i].age);
+        for (Student student : students) {
+            System.out.println("姓名：" + student.name);
+            System.out.println("年龄：" + student.age);
         }
     }
 }
+
